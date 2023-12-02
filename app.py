e5c178b48115288a70523e883e0e5cd88e6fe4ef
Tech-Vexy <146930478+Tech-Vexy@users.noreply.github.com>
2023-12-02 08:56:17 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template
+# app.py
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from firebase_admin import initialize_app, storage
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+# Initialize Firebase
+cred = None  # Add your Firebase Admin SDK credentials
+initialize_app(cred, {'storageBucket': 'your-firebase-storage-bucket'})
+
+# Endpoint to upload an image
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image provided'}), 400
+
+    image = request.files['image']
+    image_path = f'images/{image.filename}'
+
+    # Upload image to Firebase Storage
+    bucket = storage.bucket()
+    blob = bucket.blob(image_path)
+    blob.upload_from_file(image)
+
+    # Get the public URL of the uploaded image
+    image_url = blob.public_url
+
+    return jsonify({'image_url': image_url}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
